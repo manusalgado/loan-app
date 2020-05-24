487b128dea4875b3474fdd5dd606597b292d7f64
Manuel Salgado <manuel.salgado@rappi.com>
2020-05-24 17:38:20 -0500

    Added users module and ngrx entities

---
+++ src/app/modules/users/users.container.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.container.html',
+  styleUrls: ['./users.container.sass']
+})
+export class UsersContainer implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
