f6f31ab133ac3b109c40f2f85ce16d7869d52d47
Manuel Salgado <manuel.salgado@rappi.com>
2020-05-23 19:42:07 -0500

    Added Loan request module and install ngrx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'loan-request',
+    loadChildren: () =>
+    import('./modules/loan-request/loan-request.module').then((m) => m.LoanRequestModule)
+  },
+  {
+    path: '',
+    loadChildren: () =>
+    import('./modules/loan-request/loan-request.module').then((m) => m.LoanRequestModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
